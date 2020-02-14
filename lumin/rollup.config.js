a671b875b33a73c6b085e9c073a9d11eebef6cc4
panwrona <mariusz.brona@gmail.com>
2020-02-14 13:01:21 +0100

    Add typescript config files

---
+++ lumin/rollup.config.js
@@ -0,0 +1,60 @@
+import babel from 'rollup-plugin-babel';
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import copy from 'rollup-plugin-copy';
+import typescript from 'rollup-plugin-typescript';
+
+let includePathOptions = {
+  include: {},
+  paths: ['./src', '../src'],
+  external: []
+};
+
+const common = {
+  plugins: [
+    copy({
+      targets: [{ src: '../src', dest: '.' }],
+      hook: 'first'
+    }),
+    babel({
+      exclude: 'node_modules/**',
+      extensions: ['.js', '.jsx', '.ts', '.tsx']
+    }),
+    resolve({
+      customResolveOptions: {
+        moduleDirectory: '../node_modules'
+      },
+      dedupe: ['react']
+    }),
+    commonjs(),
+    typescript()
+  ]
+};
+
+export default [
+  // Build for MagicScript on LuminOS
+  {
+    ...common,
+    external: ['uv', 'lumin', 'ssl', 'jpeg', 'png', 'gl'],
+    input: 'src/main.tsx',
+    preserveModules: true,
+    output: {
+      dir: 'bin',
+      format: 'es'
+    }
+  },
+  // Build for MagicScript on Magicverse (iOS, Android)
+  {
+    ...common,
+    input: '../src/app.tsx',
+    external: ['react'],
+    output: {
+      globals: {
+        'react': 'React'
+      },
+      file: '../reactnative/bin/bundle.js',
+      format: 'iife',
+      name: '_'
+    }
+  }
+];
